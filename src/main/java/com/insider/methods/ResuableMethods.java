53adcd84439d4bc5ec23794e886519684bb89e9f
burcuonal <burcu.onal@testinium.com>
2025-03-18 22:30:17 +0300

    BaseTest ConfigReader LocatoHelper added

--- src/main/java/com/insider/methods/ResuableMethods.java
+++ src/main/java/com/insider/methods/ResuableMethods.java
@@ -1,4 +1,5 @@
 package com.insider.methods;
 
 public class ResuableMethods {
+
 }
--- src/main/java/com/insider/utils/Main.java
+++
@@ -1,7 +0,0 @@
-package com.insider.utils;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
